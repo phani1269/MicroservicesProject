b4dd89d8272d40b650d89133584efcf4ee639010
phani1269 <106523893+phani1269@users.noreply.github.com>
2022-09-08 21:01:44 +0530

    Catalog and Basket

---
+++ src/Services/Basket/Basket.API/Repositories/IBasketRepository.cs
@@ -0,0 +1,16 @@
+﻿using Basket.API.Enitities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.API.Repositories
+{
+    public  interface IBasketRepository
+    {
+        Task<ShoppingCart> GetBasket(string userName);
+        Task<ShoppingCart> UpdateBasket(ShoppingCart basket);
+        Task DeleteBasket(string userName);
+
+    }
+}
